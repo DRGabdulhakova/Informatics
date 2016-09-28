17c2de694645dbab8329f4aa4a397511a289b9e8
Dina Gabdulhakova <dinrey7@gmail.com>
2016-09-28 16:52:28 +0300

    Add files via upload

---
+++ 22-09/Ex1.java
@@ -0,0 +1,15 @@
+public class Ex1 {
+
+	public static void main (String[] args) {
+		
+		int a=5;
+		int b=7;
+		
+		b=a+b;
+		a=b-a;
+		b=b-a;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
