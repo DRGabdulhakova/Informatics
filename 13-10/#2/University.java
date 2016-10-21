6070579b7e1556c01fea3fc831fe9c55fa992d60
Dina Gabdulhakova <dinrey7@gmail.com>
2016-10-21 19:09:42 +0300

    Update University.java

--- 13-10/#2/University.java
+++ 13-10/#2/University.java
@@ -2,5 +2,11 @@ public class University {
 	
 	public static void main(String[] args) {
 		Student S = new Student();
+		Committee c = new Committee();
+		MedicalGroup mg = new MedicalGroup();
+		System.out.println(s.getMedGroup());
+		System.out.println(s.stipend());
+		System.out.println(s.setCommittee());
+		
 	}
 }
