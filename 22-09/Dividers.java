8c210f008d470383d6340391ebc993cf03a7f026
Dina Gabdulhakova <dinrey7@gmail.com>
2016-11-22 19:43:00 +0300

    Update Dividers.java

--- 22-09/Dividers.java
+++ 22-09/Dividers.java
@@ -5,11 +5,11 @@ public class Dividers {
 
 		for (int i = 1; i <= n / 2; i++) {
 			if (n % i == 0) {
-				System.out.println("i");
+				System.out.println(i);
 			}
 		}
 
-		System.out.println(n`);
+		System.out.println(n);
 	}
 
 }
