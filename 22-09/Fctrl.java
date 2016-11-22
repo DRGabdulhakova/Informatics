5f7d2be6126c8dcf1f1a534720745bc27d676ff1
Dina Gabdulhakova <dinrey7@gmail.com>
2016-11-22 21:21:05 +0300

    Update Fctrl.java

--- 22-09/Fctrl.java
+++ 22-09/Fctrl.java
@@ -3,18 +3,23 @@ public class Fctrl {
 	public static void main (String[] args) {
 		int n = Integer.parseInt(args[0]);
 		int fact = 1;
-		if (n==0) {
+		
+		if (n < 0) {
+			System.out.println("The number must be greater than or equal to 0");
+		}
+		
+		if (n == 0) {
 			
 			System.out.println(1);
 		}
 		else {
 
-		for (int i=1; i<=n; i++) {
-			fact=fact*i;
+			for (int i = 1; i <= n; i++) {
+			fact = fact * i;
 			
 			System.out.println(fact);
 
-		}
+			}
 		}
 	}
 
