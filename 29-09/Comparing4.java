de611a26a930b4d4c8b57967fa5ec8e953cd0bc2
Dina Gabdulhakova <dinrey7@gmail.com>
2016-11-22 21:02:42 +0300

    Update Comparing4.java

--- 29-09/Comparing4.java
+++ 29-09/Comparing4.java
@@ -1,25 +1,40 @@
 public class Comparing4 {
 
-  public static void main(String []args) {
+    public static void main(String []args) {
 
-    String line = args [0];
-    String line2 = args [1];
+        String line = args [0];
+        String line2 = args [1];
 
-    char [] symb = line.toCharArray();
-    char [] symb2 = line2.toCharArray();
+        char [] symb = line.toCharArray();
+        char [] symb2 = line2.toCharArray();
 
-    boolean t = true;
+        boolean t = true;
 
-    for (int i = 0; i < symb.length; i++) {
+        for (int i = 0; i < symb.length; i++) {
 
-      if (symb[i] == symb2[i]) {
-        t = true;
-      } else {
-        t = false; 
-      
-      break;
-      }
+            if (symb[i] == symb2[i]) {
+                t = true;
+                System.out.println(line + " = " + line2);
+            } 
+            else {
+                t = false; 
+                System.out.println(line + " != " + line2);
+                break;
+            }
+        }
+        
+        for (int i = 0; i < symb.length; i++) {
+			      if (line.charAt(i) > line2.charAt(i)) {
+				        System.out.println(line + " > " + line2);
+				        t = false;
+				        break;
+			      }
+			      else if (line.charAt(i) < line2.charAt(i)) {
+				        System.out.println(line + " < " + line2);
+				        t = false;
+				        break;
+			      }
+		    }
+    
     }
-      System.out.println(t);
-  }
 }
